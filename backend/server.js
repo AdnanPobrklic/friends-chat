337c0d04b1efe53fcd03533416618b036ee4711f
Adnan Pobrklic <adn.pobrklic@gmail.com>
2024-03-15 14:09:30 +0100

    adsa

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,11 @@ const sessionMiddleware = session({
     resave: true,
     saveUninitialized: false,
     cookie: { 
-        sameSite: 'none', 
+        sameSite: 'none',
+        secure: true,
+        httpOnly: true, 
         partitioned: true,
+        maxAge: (7 * 24 * 60 * 60 * 1000)
     },
     store: MongoStore.create({ mongoUrl: dbURI }),
 });
